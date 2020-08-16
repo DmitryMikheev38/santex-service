1660c9c9866632145e8c97bcc8550098a5181dd8
dmikheev <dima.m1993@mail.ru>
2020-08-16 13:32:00 +0300

    dev

--- resources/views/landing.blade.php
+++ resources/views/landing.blade.php
@@ -7,7 +7,8 @@
         <title>Сантех-сервис</title>
         <link rel="stylesheet" href="./css/app.css">
         <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+        <!-- <link href="https://fonts.googleapis.com/css?family=Open-sans:200,600" rel="stylesheet"> -->
+        <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;700&display=swap" rel="stylesheet">
     </head>
     <body>
 
@@ -15,17 +16,18 @@
         <div class="slider header__slider" data-images="santex-image-1.jpg,santex-image-2.jpg,santex-image-3.jpg">
         </div>
         <div class="wrapper header__wrapper">
-            <h1 class="title header__title">РАБОТЫ ПО МОНТАЖУ САНТЕХНИЧЕСКИХ СИСТЕМ В г. КРАСНОДАР</h1>
+            <h1 class="h1 header__h1">РАБОТЫ ПО МОНТАЖУ САНТЕХНИЧЕСКИХ СИСТЕМ В г. КРАСНОДАР</h1>
         </div>
+        <div class="line"></div>
     </div>
 
     <div class="content">
         <div class="wrapper content__wrapper">
-            <h2 class="title-h2 content__title-h2">МОНТАЖ СИСТЕМ ОТОПЛЕНИЯ И ВОДОСНАБЖЕНИЯ В КВАРТИРАХ И ЧАСТНЫХ ДОМАХ.</h2>
-            <h3 class="title-h3 content__title-h3">ПЕРЕЧЕНЬ РАБОТ:</h3>
-            <ul class="service-list content__service-list">
+            <h2 class="h2 content__h2">МОНТАЖ СИСТЕМ ОТОПЛЕНИЯ И ВОДОСНАБЖЕНИЯ В КВАРТИРАХ И ЧАСТНЫХ ДОМАХ.</h2>
+            <h3 class="h3 content__h3">ПЕРЕЧЕНЬ РАБОТ:</h3>
+            <ul class="list content__list">
                 <li>Монтаж отопления, установка радиаторов</li>
-                <li>Монтаж водоснабжения, разводка холодной и горячей воды</li>
+                <li>Монтаж водоснабжеия, разводка холодной и горячей воды</li>
                 <li>Монтаж внутренней и наружной канализации</li>
                 <li>Монтаж теплых полов</li>
                 <li>Монтаж магистралей отопления и ГВС в цоколе и тех.этаже(чердак)</li>
@@ -33,22 +35,25 @@
                 <li>Замена труб водоснабжения и канализации</li>
                 <li>Установка унитазов, ванн, душевых кабин, моек, умывальников, смесителей</li>
             </ul>
-            <p>
+            <p class="info content__info">
                 БРИГАДА КВАЛИФИЦИРОВАННЫХ РАБОЧИХ ОКАЖЕТ КАЧЕСТВЕННЫЕ УСЛУГИ С ПРЕДОСТАВЛЕНИЕМ ГАРАНТИЙ НА ВСЕ ВИДЫ РАБОТ.
                 ВЫПОЛНЯЕМ РАЗВОДКУ СТАЛЬНЫМИ , МЕТАЛЛОПЛАСТИКОВЫМИ, ПОЛИПРОПИЛЕНОВЫМИ ТРУБАМИ.
                 ВЫПОЛНЯЕМ МОНТАЖ КАНАЛИЗАЦИИ ИЗ ПОЛИПРОПИЛЕНОВЫХ И ЧУГУННЫХ ТРУБ.
             </p>
-            <ul class="contact-list content__contact-list">
+
+            <h3 class="h3 content__h3">КОНТАКТЫ:</h3>
+            <ul class="list content__list">
                 <li>+79182621265</li>
                 <li>+79180289315</li>
                 <li>+79676718400</li>
             </ul>
         </div>
+        <div class="line"></div>
     </div>
 
     <footer class="footer">
         <div class="wrapper">
-            <span>г. Краснодар {{  date("Y") }}</span>
+            <span class="footer-date">г. Краснодар {{  date("Y") }} год.</span>
         </div>
     </footer>
 
