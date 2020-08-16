dcf7f8985ee2b613d40ddd3b89f398ba716405b9
dmikheev <dima.m1993@mail.ru>
2020-08-16 18:12:20 +0300

    add style

--- resources/views/landing.blade.php
+++ resources/views/landing.blade.php
@@ -1,10 +1,19 @@
 <!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<html lang="ru">
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Сантех-сервис</title>
+        <title>Сантех-сервис г. Краснодар</title>
+        <meta
+            name="description"
+            content="РАБОТЫ ПО МОНТАЖУ САНТЕХНИЧЕСКИХ СИСТЕМ В г. КРАСНОДАР.
+                    МОНТАЖ СИСТЕМ ОТОПЛЕНИЯ И ВОДОСНАБЖЕНИЯ В КВАРТИРАХ И ЧАСТНЫХ ДОМАХ.
+                    БРИГАДА КВАЛИФИЦИРОВАННЫХ РАБОЧИХ ОКАЖЕТ КАЧЕСТВЕННЫЕ УСЛУГИ С ПРЕДОСТАВЛЕНИЕМ ГАРАНТИЙ НА ВСЕ ВИДЫ РАБОТ.
+                    ВЫПОЛНЯЕМ РАЗВОДКУ СТАЛЬНЫМИ , МЕТАЛЛОПЛАСТИКОВЫМИ, ПОЛИПРОПИЛЕНОВЫМИ ТРУБАМИ.
+                    ВЫПОЛНЯЕМ МОНТАЖ КАНАЛИЗАЦИИ ИЗ ПОЛИПРОПИЛЕНОВЫХ И ЧУГУННЫХ ТРУБ."
+        >
+        <meta name="keywords" content="ремонт,сантехника,Краснодар,замена,трубы,котлы,монтаж,отопление,канализация,водоснабжение" />
         <link rel="stylesheet" href="./css/app.css">
         <!-- Fonts -->
         <!-- <link href="https://fonts.googleapis.com/css?family=Open-sans:200,600" rel="stylesheet"> -->
@@ -21,11 +30,15 @@
         <div class="line"></div>
     </div>
 
-    <div class="content">
-        <div class="wrapper content__wrapper">
-            <h2 class="h2 content__h2">МОНТАЖ СИСТЕМ ОТОПЛЕНИЯ И ВОДОСНАБЖЕНИЯ В КВАРТИРАХ И ЧАСТНЫХ ДОМАХ.</h2>
-            <h3 class="h3 content__h3">ПЕРЕЧЕНЬ РАБОТ:</h3>
-            <ul class="list content__list">
+    <div class="landing">
+        <div class="wrapper landing__wrapper">
+            <h2 class="h2 landing__h2">МОНТАЖ СИСТЕМ ОТОПЛЕНИЯ И ВОДОСНАБЖЕНИЯ В КВАРТИРАХ И ЧАСТНЫХ ДОМАХ.</h2>
+            <div class="title-block">
+                <object class="landing__icon" type="image/svg+xml" data="./images/build-24px.svg" width="35" height="35"></object>
+                <h3 class="h3 landing__h3">ПЕРЕЧЕНЬ РАБОТ:</h3>
+            </div>
+
+            <ul class="list landing__list">
                 <li>Монтаж отопления, установка радиаторов</li>
                 <li>Монтаж водоснабжеия, разводка холодной и горячей воды</li>
                 <li>Монтаж внутренней и наружной канализации</li>
@@ -35,14 +48,23 @@
                 <li>Замена труб водоснабжения и канализации</li>
                 <li>Установка унитазов, ванн, душевых кабин, моек, умывальников, смесителей</li>
             </ul>
-            <p class="info content__info">
-                БРИГАДА КВАЛИФИЦИРОВАННЫХ РАБОЧИХ ОКАЖЕТ КАЧЕСТВЕННЫЕ УСЛУГИ С ПРЕДОСТАВЛЕНИЕМ ГАРАНТИЙ НА ВСЕ ВИДЫ РАБОТ.
-                ВЫПОЛНЯЕМ РАЗВОДКУ СТАЛЬНЫМИ , МЕТАЛЛОПЛАСТИКОВЫМИ, ПОЛИПРОПИЛЕНОВЫМИ ТРУБАМИ.
-                ВЫПОЛНЯЕМ МОНТАЖ КАНАЛИЗАЦИИ ИЗ ПОЛИПРОПИЛЕНОВЫХ И ЧУГУННЫХ ТРУБ.
-            </p>
 
-            <h3 class="h3 content__h3">КОНТАКТЫ:</h3>
-            <ul class="list content__list">
+            <div class="info-block">
+                <object type="image/svg+xml" data="./images/feedback-24px.svg" width="60" height="60"></object>
+                <p class="info landing__info">
+                    БРИГАДА КВАЛИФИЦИРОВАННЫХ РАБОЧИХ ОКАЖЕТ КАЧЕСТВЕННЫЕ УСЛУГИ С ПРЕДОСТАВЛЕНИЕМ ГАРАНТИЙ НА ВСЕ ВИДЫ РАБОТ.
+                    ВЫПОЛНЯЕМ РАЗВОДКУ СТАЛЬНЫМИ , МЕТАЛЛОПЛАСТИКОВЫМИ, ПОЛИПРОПИЛЕНОВЫМИ ТРУБАМИ.
+                    ВЫПОЛНЯЕМ МОНТАЖ КАНАЛИЗАЦИИ ИЗ ПОЛИПРОПИЛЕНОВЫХ И ЧУГУННЫХ ТРУБ.
+                </p>
+            </div>
+
+
+            <div class="title-block">
+                <object class="landing__icon" type="image/svg+xml" data="./images/call-24px.svg" width="35" height="35"></object>
+                <h3 class="h3 landing__h3">КОНТАКТЫ:</h3>
+            </div>
+            <!-- <h3 class="h3 content__h3">КОНТАКТЫ:</h3> -->
+            <ul class="list landing__list">
                 <li>+79182621265</li>
                 <li>+79180289315</li>
                 <li>+79676718400</li>
